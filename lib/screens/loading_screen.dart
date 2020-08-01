e984133ffa7527f9a78195323babb8563f19e782
Haardik Dharma <dharmahaardik08@gmail.com>
2020-08-01 16:36:46 +0530

    Add SpinKit widget

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,9 @@
+import 'package:clima/screens/location_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
 import 'package:clima/services/networking.dart';
+import 'location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 const apiKey = 'a04f34622a3860b708b1ee6da83844fc';
 
@@ -34,10 +37,16 @@ class _LoadingScreenState extends State<LoadingScreen> {
     NetworkHelper networkHelper = NetworkHelper('http://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
 
       var weatherData = await networkHelper.getData();
+
+      Navigator.push(context, MaterialPageRoute(builder: (context){
+        return LocationScreen();
+      }));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(child: SpinKitThreeBounce(color: Colors.white, size: 80.0,),),
+    );
   }
 }
